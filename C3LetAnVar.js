728278530bc35d862da395a51ea3363078a59a91
Jimil Hingu <jimilhingu@gmail.com>
2023-07-22 21:36:57 -0700

    22 july 2023

---
+++ C3LetAnVar.js
@@ -0,0 +1,8 @@
+//try to make let as it more logical
+var a = "jimil"
+
+{
+    let a = "44"   //let allow me to make a variabel and change his vakue for one block
+    console.log(a)
+}
+console.log(a)
